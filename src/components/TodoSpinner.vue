bd14b5161bbdcef621d5e03faba05c161823b4e2
frontend <gabriellennon@wizsolucoes.com.br>
2022-08-30 22:02:53 -0300

    integration with back-end and install vuex

---
+++ src/components/TodoSpinner.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="text-center">
+        <img
+            src="@/assets/img/spinner.svg"
+            alt="loading"
+            class="inline-block w-5 h-5"
+        >
+    </div>
+</template>
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    todos: []
+  },
+  getters: {
+  },
+  mutations: {
+    storeTodos(state, payload) {
+      state.todos = payload
+    }
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
